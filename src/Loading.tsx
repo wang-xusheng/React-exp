3b2f4c5d81862afa03792999cc45629d2463235a
wangxusheng3 <wangxusheng3@xiaomi.com>
2024-09-18 18:43:48 +0800

    feat: UpdateName

---
+++ src/Loading.tsx
@@ -0,0 +1,33 @@
+import { useState, useTransition } from "react"
+
+async function send(message:string) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return {message, sending: false, code: Math.random() > 0.5 ? 0 : 1};
+  }
+function UpdateName() {
+    const [name, setName] = useState("");
+    const [error, setError] = useState<string | null>(null);
+    const [isPending, startTransition] = useTransition();
+  
+    const handleSubmit = () => {
+      startTransition(async () => {
+        const res = await send(name);
+        if (res.code !== 0) {
+          setError(res.message);
+        } else {
+          setError(null);
+        }
+      })
+    };
+  
+    return (
+      <div>
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+        <button onClick={handleSubmit} disabled={isPending} style={{ margin:10, backgroundColor: isPending ? 'gray' : 'green', color: 'white'}}>
+          Update
+        </button>
+        {error && <p>提交错误</p>}
+      </div>
+    );
+  }
+export default UpdateName
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import Count from './Count.tsx'
 import Todos from './Todos.tsx'
+import UpdateName from './Loading.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App />
-    <Count></Count> */}
-    <Todos></Todos>
+    {/* <App /> */}
+    {/* <Count></Count> */}
+    {/* <Todos></Todos> */}
+    {<UpdateName></UpdateName>}
   </StrictMode>,
 )

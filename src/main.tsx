928ffc3eb59536a18d0c72577e465049d8fd4d8b
wangxusheng3 <wangxusheng3@xiaomi.com>
2024-09-25 19:19:58 +0800

    feat: 优化

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import Count from './Count.tsx'
-import Todos from './Todos.tsx'
 import UpdateName from './Loading.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <Count></Count> */}
-    {/* <Todos></Todos> */}
-    {<UpdateName></UpdateName>}
+    <UpdateName></UpdateName>
   </StrictMode>,
 )
